b291e8a700c4f4fcd1c662ada21abb53d95f3f89
kewapo <kewapo@gmail.com>
2017-12-18 14:03:47 +0100

    guarda archivos, lee archivos y los copara

--- ejemplo.py
+++ ejemplo.py
@@ -1,11 +1,11 @@
 # Autor: kewapo
 import os.path
 import datetime
-import csv
 
 ruta = r'E:\libros'
 
-def guardar_archivos(nombre_fichero):
+
+def guardar_archivos(ruta, nombre_fichero):
     print('Leyendo archivos del directorio ' + ruta + ' ...')
     with open(nombre_fichero, 'w', encoding='latin_1', errors='ignore') as archivo:
         archivo.write('Directorio;Nombre;Tamaño;Fecha de creación\n')
@@ -19,4 +19,27 @@ def guardar_archivos(nombre_fichero):
                 archivo.write('\n')
     print('Escribiendo datos en ' + nombre_fichero)
 
-if __name__ == '__main__': guardar_archivos('archivos.csv')
+
+def leer_archivos(nombre_fichero):
+    archivos = set()
+    with open(nombre_fichero) as archivo:
+        for linea in archivo:
+            directorio = linea.split(';')[0]
+            nombre = linea.split(';')[1]
+            archivos.add(os.path.join(directorio, nombre))
+    return archivos
+
+'''
+He añadido un archivo nuevo a uno de los directorios
+Debería immprimirse con este código
+'''
+if __name__ == '__main__':
+    archivos = leer_archivos('archivos.csv')
+    guardar_archivos(r'E:\libros', 'archivos1.csv')
+    with open('archivos1.csv') as a:
+        for linea in a:
+            directorio = linea.split(';')[0]
+            nombre = linea.split(';')[1]
+            todo = os.path.join(directorio, nombre)
+            if todo not in archivos:
+                print(todo)
